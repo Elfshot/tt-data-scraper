1b2d77dc3f80f6f7f19811e46d4bc63e9f3a3088
Elfshot <44043197+Elfshot@users.noreply.github.com>
2021-05-02 17:44:03 -0400

    debug

--- src/index.js
+++ src/index.js
@@ -57,7 +57,8 @@ async function writeUsers(users={Type: Object}) {
             var old = await Model.findOne({ vrpId: id }).exec();
             if (old) await Model.findOneAndUpdate({ _id: old._id }, { userName: name, countFound: old.countFound + 1 , lastFound: date}, {useFindAndModify: false}, 
                 ((err, result) => { 
-                    if (err) console.log(err);
+                    //if (err) console.log(err);
+                    console.log(err ? err: result)
                     if (i+1 == playerIds.length) db.disconnect(() => { console.log("disconnected") });
                 })
             );
@@ -70,7 +71,8 @@ async function writeUsers(users={Type: Object}) {
                     lastFound: date,
                 })
                 await userModel.save((err, result) => { 
-                    if (err) console.log(err);
+                    //if (err) console.log(err);
+                    console.log(err ? err: result)
                     if (i+1 == playerIds.length) db.disconnect(() => { console.log("disconnected") });
             })};
         }
